fa0ec6f7d53ec666c409510e1bf276bf096875e9
Luke Yarnall <lukeyarnall@gmail.com>
2018-10-22 17:55:30 -0400

    Add example DSC configuration for single and multiple PI tags

---
+++ Examples/xPIAccessControl_AddPITagAccessControlExample.ps1
@@ -0,0 +1,42 @@
+Configuration AddPITagAccessControlExample
+{
+    Import-DscResource -Module PISecurityDSC
+    
+    # For use below in the multi-tag example.
+    Import-Module OSIsoft.PowerShell
+    $connection = Connect-PIDataArchive localhost
+    $tags = Get-PIPoint -Connection $connection -WhereClause "description:=*InternalCompanyUse*"
+    $suffix = "_datasecurity"
+
+    Node localhost
+    {
+        # A single tag can be specified in a straightforward manner.
+        PIAccessControl PIAccessControl_SingleTag
+        {
+            Name = "ContractorsAndEmployeesTag"
+            Identity = "Contractors"
+            Type = "ptsecurity"
+            Access = "Read"
+            Ensure = "Present"
+        }
+
+        # Alternatively, multiple tags can be specified by using a foreach loop.
+        # In this example, it is ensured that no tags designated for InternalCompanyUse are readable by Contractors.
+        # This generates a set of PIAccessControls at the time of compilation; it does not automatically update when new tags are added.
+        #      When new InternalCompanyUse tags are added, the DSC script should be recompiled.
+        foreach ($tag in $tags) {
+            $resourceName = "$($tag.Point.Name)$suffix"
+            PIAccessControl $resourceName
+            {
+                Name = $($tag.Point.Name)
+                Identity = "Contractors"
+                Type = "datasecurity"
+                Access = ""
+                Ensure = "Present"
+            }
+        }
+    }
+
+    # Cleanly disconnect from PIDataArchive
+    Disconnect-PIDataArchive -Connection $Connection
+}
